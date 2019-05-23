4f34c370e7f4c0eec8922d6a878cc75084a69b8d
= <=>
2019-05-23 18:18:29 +0530

    Dashboard changes

--- src/assets/javascript/custom.js
+++ src/assets/javascript/custom.js
@@ -2,6 +2,25 @@ function documentReady()
  {
      $('#sidebarCollapse').on('click', function () {
                 $('#sidebar').toggleClass('active');
+                $(this).toggleClass('active'); 
+                
             });
             
 };
+
+
+
+// <!-- 
+// <script type="text/javascript"> 
+//   $(document).ready(function () {
+//     $('#sidebarCollapse').on('click', function () {
+//       console.log("Welcome");
+//       $('#sidebar').toggleClass('active');
+//       $('#sidebarCollapse').toggleClass('active'); 
+     
+//       // $(this).toggleClass('active');
+      
+//     });
+//   });
+
+// </script> -->
