0d2c078940af9b903ba76242fb674b1c6e35ca83
Anuradha <vaka.anuradha@gmail.com>
2019-05-24 14:47:03 +0530

    Added Questions Model

---
+++ backend/models/question.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+import { AnswerOptionSchema } from './answer-option-schema';
+const uniqueValidator = require("mongoose-unique-validator");
+
+export const QuestionSchema = mongoose.Schema({
+  quesid:         { type: String},
+  questype:       { type: String},
+  quesCat:        { type: String},
+  quesSubCat:     { type: String},
+  question:       { type: String, minlength: 10, maxlength: 1000, required: true},
+  quesFormatted:  { type: String, minlength: 10, maxlength: 1000},
+  answerOptions:  { type: [AnswerOptionSchema], default: undefined,
+                    validate: { validator: function(value) {return value && value.length === 4;},
+                    message: 'Answer options should be 4.'}
+                  },
+  Reason:         { type: String},
+  },
+{
+  timestamps: true
+});
+
+export const Question = mongoose.model('Question', QuestionSchema);
